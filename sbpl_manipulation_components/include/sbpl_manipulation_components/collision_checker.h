f2e43e8a635afc34f032a7a9990c4853dc1d65c5
Ben Cohen <bcohen@bean2.(none)>
2013-06-12 16:51:13 -0400

    Added a few functions to collision checker

--- sbpl_manipulation_components/include/sbpl_manipulation_components/collision_checker.h
+++ sbpl_manipulation_components/include/sbpl_manipulation_components/collision_checker.h
@@ -5,6 +5,8 @@
 #include <angles/angles.h>
 #include <string>
 #include <sbpl_geometry_utils/Interpolator.h>
+#include <arm_navigation_msgs/PlanningScene.h>
+#include <visualization_msgs/MarkerArray.h>
 
 namespace sbpl_arm_planner {
 
@@ -21,6 +23,11 @@ class CollisionChecker
 
     virtual void setPlanningJoints(const std::vector<std::string> &planning_joints);
 
+    /* World Update */
+    virtual void setRobotState(const arm_navigation_msgs::RobotState &state);
+
+    virtual void setPlanningScene(const arm_navigation_msgs::PlanningScene &scene);
+
     /* Collision Checking */
     virtual bool isStateValid(const std::vector<double> &angles, bool verbose, bool visualize, double &dist);
    
@@ -29,6 +36,11 @@ class CollisionChecker
     /* Utils */
     virtual bool interpolatePath(const std::vector<double> &start, const std::vector<double> &end, const std::vector<double> &inc, std::vector<std::vector<double> >& path);
 
+    /* Visualizations */
+    virtual visualization_msgs::Marker getMarker(std::string type);
+    
+    virtual visualization_msgs::MarkerArray getMarkerArray(std::string type);
+  
   protected:
 
     std::string group_name_;
